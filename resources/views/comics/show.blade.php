3faf834c00222ac964f4091ab1e826deece5c4ca
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2022-12-17 14:45:51 +0100

    add style to home/comics/singleComic

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -6,7 +6,10 @@
     <section>
         <div class="container">
             <img src="{{ $singleComic['thumb'] }}" alt="{{ $singleComic['title'] }}">
-            <h2>{{ $singleComic['title'] }}</h2>
+            <h2 class="text-light">{{ $singleComic['title'] }}</h2>
+            <h4 class="text-light">{{ $singleComic['sale_date'] }}</h4>
+            <h4 class="text-light">{{ $singleComic['description'] }}</h4>
+
         </div>
     </section>
 @endsection
